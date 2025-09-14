2dfd5c3abeefc9c90db1c07ab13913be83b32403
Noah <noah.fraiture@guardsquare.com>
2025-09-14 21:21:03 +0200

    execute actual command

--- command.go
+++ command.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"app/tui"
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 type command string
@@ -15,18 +19,23 @@ const (
 type commandPanel struct {
 	*tui.ListPanel[command]
 	from *commit
-	to   *commit
+	on   *commit
 	diff *diff
+	msg  *string
 }
 
 func (cp *commandPanel) Update(msg tui.InputMessage) bool {
 	if msg.IsKey(tui.KeyEnter) {
-		panic(fmt.Sprintln(cp.from, cp.to, cp.diff))
+		err := cp.runCmd()
+		if err != nil {
+			panic(err)
+		}
+		os.Exit(0)
 	}
 	return cp.ListPanel.Update(msg)
 }
 
-func NewCmdPanel(name string, from, to *commit, diff *diff) commandPanel {
+func NewCmdPanel(name string, from, on *commit, diff *diff, msg *string) commandPanel {
 	return commandPanel{
 		ListPanel: &tui.ListPanel[command]{
 			PanelBase: tui.PanelBase{
@@ -36,7 +45,25 @@ func NewCmdPanel(name string, from, to *commit, diff *diff) commandPanel {
 			Items: []command{Update, Create},
 		},
 		from: from,
-		to:   to,
+		on:   on,
 		diff: diff,
+		msg:  msg,
 	}
+}
+
+func (cp *commandPanel) runCmd() error {
+	patch, err := cp.from.Patch(cp.on.Commit)
+	if err != nil {
+		return fmt.Errorf("failed to get patch from commits")
+	}
+	patcherReader := strings.NewReader(patch.String())
+	switch cp.Items[cp.Selected] {
+	case Create:
+	case Update:
+		// TODO : checkout
+		cmd := exec.Command("arc", "diff", "--raw", "--update", cp.diff.id, "--message", *cp.msg)
+		cmd.Stdin = patcherReader
+		return cmd.Run()
+	}
+	return errors.New("invalid command")
 }
